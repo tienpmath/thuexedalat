65a52b0d7c40277b0836488d9fc7707fc073f8d2
tienpmath <designdalat@gmail.com>
2025-01-09 11:59:42 +0700

    meta

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,7 @@ const outfit = Outfit({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://www.dulichtrungthanhdalat.com"),
   title: "Thuê xe 4 chỗ, 5 chỗ, 7 chỗ, 16 chỗ, 29 chỗ Đà Lạt",
   description:
     "Dịch vụ Thuê xe 4 chỗ, 5 chỗ, 7 chỗ, 16 chỗ, 29 chỗ Đà Lạt. DỊCH VỤ THUÊ XE Ô TÔ ĐÀ LẠT (CÓ TÀI XẾ); Thuê xe ô tô Đà Lạt tham quan các địa điểm theo yêu cầu",
